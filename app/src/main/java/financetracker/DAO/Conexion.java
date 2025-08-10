6d230061f771f0b1e0d84f556f6a3b78f8ceb83c
Hugo Montes <huugomontees@gmail.com>
2025-08-10 12:03:50 +0200

    Add conexion to db

---
+++ app/src/main/java/financetracker/DAO/Conexion.java
@@ -0,0 +1,14 @@
+package financetracker.DAO;
+
+import java.sql.Connection;
+
+public class Conexion {
+    String url;
+    String user;
+    String password;
+    public Connection client;
+    
+    public Conexion(){
+
+    }
+}
