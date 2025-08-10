e95375e287df67d3c1cecac54fa795ad99856f1f
Hugo Montes <huugomontees@gmail.com>
2025-08-10 12:10:19 +0200

    Add fn to class Conexion

--- app/src/main/java/financetracker/DAO/Conexion.java
+++ app/src/main/java/financetracker/DAO/Conexion.java
@@ -1,6 +1,8 @@
 package financetracker.DAO;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
     String url;
@@ -8,7 +10,21 @@ public class Conexion {
     String password;
     public Connection client;
     
-    public Conexion(){
+    public Conexion(String url, String user, String password){
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
 
+    public void initConexion() throws SQLException{
+        if (client == null) {
+            client = DriverManager.getConnection(url, user, password);
+            System.err.println("Connected");
+        }
+    }
+
+    public void stopConexion() throws SQLException{
+        client.close();
+        System.err.println("Disconnected");
     }
 }
