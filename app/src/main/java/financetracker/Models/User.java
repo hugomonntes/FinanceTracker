d0fd665f6b0a57ee15c6b84e433f4477aa33ae07
Hugo Montes <huugomontees@gmail.com>
2025-08-11 22:50:42 +0200

    Testing setters

--- app/src/main/java/financetracker/Models/User.java
+++ app/src/main/java/financetracker/Models/User.java
@@ -58,9 +58,13 @@ public class User {
     }
 
     public User(int id, String username, String email, String password) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
+        setId(id);
+        setUsername(username);
+        setEmail(email);
+        setPassword(password);
+    }
+
+    public User(){
+        
     }
 }
