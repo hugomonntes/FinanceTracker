82adedc9e9e70aa13c51722746c6e388ae28ab87
Hugo Montes <huugomontees@gmail.com>
2025-08-13 12:24:04 +0200

    Add to dao getAllItems fn

--- app/src/main/java/financetracker/DAO/UserDAO.java
+++ app/src/main/java/financetracker/DAO/UserDAO.java
@@ -1,9 +1,33 @@
 package financetracker.DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import financetracker.Models.User;
+
 public class UserDAO {
     ConnectionDB connection;
 
-    public UserDAO(ConnectionDB connection){
+    public UserDAO(ConnectionDB connection) {
         this.connection = connection;
+    }
+
+    public ArrayList<User> getAllItems() throws SQLException {
+        Statement stmt = this.connection.client.createStatement();
+        ArrayList<User> users = new ArrayList<>();
+
+        String query = "SELECT * FROM users";
+        ResultSet result = stmt.executeQuery(query);
+
+        while (result.next()) {
+            users.add(new User(result.getInt("id"), result.getString("username"), result.getString("email"),
+                    result.getString("password_hash")));
+        }
+
+        result.close();
+        stmt.close();
+        return users;
     }
 }
