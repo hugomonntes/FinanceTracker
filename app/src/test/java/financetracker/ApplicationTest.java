3ad3f2d281d6a90ef3fa4e2b917cc31f8eb9f6e2
Hugo Montes <huugomontees@gmail.com>
2025-08-26 12:26:20 +0200

    Add testing

--- app/src/test/java/financetracker/ApplicationTest.java
+++ app/src/test/java/financetracker/ApplicationTest.java
@@ -1,17 +1,21 @@
 
 package financetracker;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import financetracker.DAO.UserDAO;
+import financetracker.Models.User;
+
 @DisplayName("Application")
 public class ApplicationTest {
+  private UserDAO userDAO;
 
   @Test
-  @DisplayName("Pointless test")
-  void smokeTest() {
-    assertThat(true).isEqualTo(true);
+  void testCreateItem() throws SQLException {
+    User newUser = new User(1, "hugo", "hmontes@gmail.com", "123");
+    userDAO.createItem(newUser);
   }
 }
