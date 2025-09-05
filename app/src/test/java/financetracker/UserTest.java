19ca5d3f7d8389f32d9507e1a5245b18a232d2ce
Hugo Montes <huugomontees@gmail.com>
2025-09-05 11:36:26 +0200

    Update testing

--- app/src/test/java/financetracker/UserTest.java
+++ app/src/test/java/financetracker/UserTest.java
@@ -6,9 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import financetracker.DAO.ConnectionDB;
+import financetracker.DAO.UserDAO;
 import financetracker.Models.User;
 
 @DisplayName("Application")
@@ -25,9 +32,10 @@ public class UserTest {
   }
 
   @Test
-  void testCheckPassword() {
+  void testCheckPassword() throws SQLException {
     User fukin = new User();
-    fukin.setPassword("123");
-    assertTrue(fukin.checkPassword(fukin.getPassword()), "" + fukin.getPassword());
+    fukin.setPassword("aaa");
+    assertTrue(fukin.checkPassword("$2a$10$XNXq6ikcBvkEEG0UK4u5hepLj7VdYxSU4QkK/G298FroxQf74Pf4O"), fukin.getPassword());
   }
+
 }
